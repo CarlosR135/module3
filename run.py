d9b25227863705bc00bf0a46333f1c0a753e5e2d
Carlos Romao <carlosromao135@gmail.com>
2024-09-13 22:54:38 +0000

    Create function to update Google Sheets with the game grid

--- run.py
+++ run.py
@@ -24,7 +24,8 @@ SHEET = GSPREAD_CLIENT.open(SHEET_NAME).sheet1
 def init_grid(size):
     return [['O'] * size for _ in range(size)]
 
-    def place_ships(grid, num_ships):
+# Funtion to place ships randmomly to the grid
+def place_ships(grid, num_ships):
     ships = []
     while len(ships) < num_ships:
         x, y = random.randint(0, GRID_SIZE - 1), random.randint(0, GRID_SIZE - 1)
@@ -33,7 +34,24 @@ def init_grid(size):
             ships.append((x, y))
     return ships
 
+# Function to update the game grid n Googe Sheets
+def update_sheet(sheet,grid, start_now):
+    cell_list = sheet.range(start_row, 1, start_row + GRID_SIZE)
+    for i, cell in enumerate(cell_list):
+        row = i // GRIDE_SIZE
+        col = i % GRID_SIZE
+        cell.value = grid[row][col]
+    sheet.update_cells(cell_list)
+
+
+
     
+   
+
+
+
+
+
 
 
   
