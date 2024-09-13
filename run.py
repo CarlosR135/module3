6c3d01774852eced8807534a22f221e0e44a6278
Carlos Romao <carlosromao135@gmail.com>
2024-09-13 20:51:04 +0000

    Create function to initialize the game grid

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
 import gspread
 from google.oauth2.service_account import Credentials
 
+## Constants for grid size and number of ships
 GRID_SIZE = 5
 NUM_SHIPS = 3
-
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -11,6 +11,7 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive"
     ]
 
+# Authenticate and connect to the Google Sheet
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
@@ -18,6 +19,10 @@ SHEET = GSPREAD_CLIENT.open('module')
 
 SHEET_NAME = 'module'
 SHEET = GSPREAD_CLIENT.open(SHEET_NAME).sheet1
+
+# Funtion to initialize the game grid
+def init_grid(size):
+    return [['O'] * size for _ in range(size)]
 
 
 
